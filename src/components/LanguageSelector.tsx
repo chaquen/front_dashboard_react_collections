f1510a3cbdbd1144d3f379651a14225e9fd51bce
Adrian Guzman <edgar.guzman21@gmail.com>
2025-06-16 17:48:45 -0500

    feat: Mejorado el selector de idiomas con banderas
    - Instalado react-country-flag para iconos de banderas
    - Agregadas banderas para cada idioma (ES/EN)
    - Mejorado el diseño del selector con alineación y espaciado
    - Agregada visualización de bandera en el botón principal
    - Restructurado el componente para mejor mantenibilidad

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -1,5 +1,19 @@
 import { Dropdown } from 'react-bootstrap'
 import { useTranslation } from 'react-i18next'
+import ReactCountryFlag from 'react-country-flag'
+
+const LANGUAGES = [
+  {
+    code: 'en',
+    name: 'English',
+    countryCode: 'US',
+  },
+  {
+    code: 'es',
+    name: 'Español',
+    countryCode: 'ES',
+  },
+]
 
 const LanguageSelector = () => {
   const { i18n } = useTranslation()
@@ -8,15 +22,40 @@ const LanguageSelector = () => {
     i18n.changeLanguage(lng)
   }
 
+  const currentLang = LANGUAGES.find(lang => lang.code === i18n.language) || LANGUAGES[0]
+
   return (
     <Dropdown>
-      <Dropdown.Toggle variant="light" id="dropdown-languages">
-        {i18n.language === 'es' ? 'ES' : 'EN'}
+      <Dropdown.Toggle variant="light" id="dropdown-languages" className="d-flex align-items-center gap-1">
+        <ReactCountryFlag
+          countryCode={currentLang.countryCode}
+          svg
+          style={{
+            width: '1.2em',
+            height: '1.2em',
+          }}
+        />
+        <span className="ms-1">{currentLang.name}</span>
       </Dropdown.Toggle>
 
       <Dropdown.Menu>
-        <Dropdown.Item onClick={() => changeLanguage('en')}>English</Dropdown.Item>
-        <Dropdown.Item onClick={() => changeLanguage('es')}>Español</Dropdown.Item>
+        {LANGUAGES.map((lang) => (
+          <Dropdown.Item 
+            key={lang.code} 
+            onClick={() => changeLanguage(lang.code)}
+            className="d-flex align-items-center gap-2"
+          >
+            <ReactCountryFlag
+              countryCode={lang.countryCode}
+              svg
+              style={{
+                width: '1.2em',
+                height: '1.2em',
+              }}
+            />
+            {lang.name}
+          </Dropdown.Item>
+        ))}
       </Dropdown.Menu>
     </Dropdown>
   )
